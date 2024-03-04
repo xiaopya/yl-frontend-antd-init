60ba737ea065d93cc52876ba4c9a580b23453585
xiaopya <echo_c120@163.com>
2024-03-04 21:09:35 +0800

    :sparkles: 前端模版

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,30 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import '@umijs/max';
+import React from 'react';
+
+const Footer: React.FC = () => {
+  const defaultMessage = 'xiaopy';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      style={{
+        background: 'none',
+      }}
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'github',
+          title: (
+            <>
+              <GithubOutlined /> xiaopy
+            </>
+          ),
+          href: 'https://github.com/xiaopy',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+/**
+ * 本地后端地址
+ */
+export const BACKEND_HOST_LOCAL = "http://localhost:8080/api";
+
+/**
+ * 线上后端地址
+ */
+export const BACKEND_HOST_PROD = "https://47.98.56.224";
